059384a6b1023ad1925fbefdb0b5455d2e9ca613
Vidudaya Bandara <vnbandara@gmail.com>
2013-08-09 10:36:26 +0530

    checking mouse down event

--- Project Source Codes/Chrome Extension/Tag Highlighter/tabcolor.js
+++ Project Source Codes/Chrome Extension/Tag Highlighter/tabcolor.js
@@ -1,7 +1,9 @@
 var divs = document.getElementsByTagName("p");
 	 
 	   for(var d in divs) {
-		  divs[d].addEventListener('click',function(){this.style.background='yellow';});	
+		  //divs[d].addEventListener('click',function(){this.style.background='yellow';});	
 		  
+		  // to check mousedown behaviour , for user mousedown event
+		  divs[d].addEventListener('mousedown',function(){this.style.background='yellow'; alert('testing');});
 	  }
 	 
