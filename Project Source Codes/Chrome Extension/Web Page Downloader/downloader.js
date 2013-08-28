d86c02bf9cb51273194c62d475c46bbac17ce5f8
Vidudaya Bandara <vnbandara@gmail.com>
2013-08-28 23:24:01 +0530

    WPD : downloader.js edited

--- Project Source Codes/Chrome Extension/Web Page Downloader/downloader.js
+++ Project Source Codes/Chrome Extension/Web Page Downloader/downloader.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("download").addEventListener("click", function () {
         chrome.downloads.download({			
           "url": "http://en.wikipedia.org/wiki/Apple_Inc.",
-		 		
+		   saveAs: true
         }, function () {
             console.log("downloaded Complete");
         });
     });
-	});
+});
  
