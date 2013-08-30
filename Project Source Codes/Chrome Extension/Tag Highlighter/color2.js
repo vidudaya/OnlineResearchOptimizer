3a398d33770af9ca92ac5192617dcfa330582590
Vidudaya Bandara <vnbandara@gmail.com>
2013-08-31 00:31:53 +0530

    Update the tag highlighter code to handle multiple tag highlighting

--- Project Source Codes/Chrome Extension/Tag Highlighter/color2.js
+++ Project Source Codes/Chrome Extension/Tag Highlighter/color2.js
@@ -7,70 +7,17 @@ function clickss(e) {
 }
 
 // highlight all the p tags on user click event
- function clickcolor(e) {
+ function clickcolor() {
    chrome.tabs.executeScript(null,
                            {file : "tabcolor.js"});
   window.close();
 }
 
-function activateHighlighter(){
-	var divs = document.getElementsByTagName('*');
-	
-	for(var d in divs) { 
-			try{
-			 
-				if (divs[d].addEventListener) {
-					 divs[d].addEventListener('mouseup',function(){
-						highlight('yellow');
-					});
-				} else {
-					divs[d].attachEvent('mouseup', function(){
-						highlight('yellow');
-					});
-				} 
-			}catch(err){
-				//alert(err.message);
-			}
-	}		
-}
-
-function makeEditableAndHighlight(colour) {
-    sel = window.getSelection();
-    if (sel.rangeCount && sel.getRangeAt) {
-        range = sel.getRangeAt(0);
-    }
-    document.designMode = "on";
-    if (range) {
-        sel.removeAllRanges();
-        sel.addRange(range);
-    }
-    // Use HiliteColor since some browsers apply BackColor to the whole block
-    if (!document.execCommand("HiliteColor", false, colour)) {
-        document.execCommand("BackColor", false, colour);
-    }
-    document.designMode = "off";
-}
-function highlight(colour) {
-     var range, sel;
-    if (window.getSelection) {
-        // IE9 and non-IE
-        try {
-            if (!document.execCommand("BackColor", false, colour)) {
-                makeEditableAndHighlight(colour);
-            }
-        } catch (ex) {
-            makeEditableAndHighlight(colour)
-        }
-    }  else if (document.selection && document.selection.createRange) {
-        // IE <= 8 case
-        range = document.selection.createRange();
-        range.execCommand("BackColor", false, colour);
-    } 
-}
+ 
 
 
 document.addEventListener('DOMContentLoaded', function () {      
 	 //clickss();	 
-	 //clickcolor();
-	 activateHighlighter();
+	 clickcolor();
+	 //activateHighlighter();
 });
--- Project Source Codes/Chrome Extension/Tag Highlighter/tabcolor.js
+++ Project Source Codes/Chrome Extension/Tag Highlighter/tabcolor.js
@@ -1,19 +1,53 @@
-var divs = document.getElementsByTagName("p");
-	 
-	   for(var d in divs) {
-		  //divs[d].addEventListener('click',function(){this.style.background='yellow';});	
-		  
-		  // to check mousedown behaviour , for user mousedown event
-		  //divs[d].addEventListener('mousedown',function(){this.style.background='yellow'; alert('testing');});
-		  
-		  // check getSelection()
-		 //divs[d].addEventListener('mouseup',function(){var str= document.getSelection() ; alert(str);});  
-		 
-		 // highlight a part of a <p> tag
-		 divs[d].addEventListener('mouseup',function(){var str= document.getSelection() ;alert(str);
-			//str=str.replace(str, 'x');
-			//this.innerHTML =this.innerHTML.replace(str,'<b><background = yellow>'+str+'</b>');
-			this.innerHTML =this.innerHTML.replace(str,'<span style="background-color: '+'yellow'+' ">'+str+'</span>');
-			});	
-	  }
-	 
+function activateHighlighter(){
+	var divs = document.getElementsByTagName('*');
+	
+	for(var d in divs) { 
+			try{
+			 
+				if (divs[d].addEventListener) {
+					 divs[d].addEventListener('mouseup',function(){
+						highlight('yellow');
+					});
+				} else {
+					divs[d].attachEvent('mouseup', function(){
+						highlight('yellow');
+					});
+				} 
+			}catch(err){
+				//alert(err.message);
+			}
+	}		
+}
+function makeEditableAndHighlight(colour) {
+    sel = window.getSelection();
+    if (sel.rangeCount && sel.getRangeAt) {
+        range = sel.getRangeAt(0);
+    }
+    document.designMode = "on";
+    if (range) {
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+    // Use HiliteColor since some browsers apply BackColor to the whole block
+    if (!document.execCommand("HiliteColor", false, colour)) {
+        document.execCommand("BackColor", false, colour);
+    }
+    document.designMode = "off";
+}
+function highlight(colour) {
+     var range, sel;
+    if (window.getSelection) {
+        // IE9 and non-IE
+        try {
+            if (!document.execCommand("BackColor", false, colour)) {
+                makeEditableAndHighlight(colour);
+            }
+        } catch (ex) {
+            makeEditableAndHighlight(colour)
+        }
+    }  else if (document.selection && document.selection.createRange) {
+        // IE <= 8 case
+        range = document.selection.createRange();
+        range.execCommand("BackColor", false, colour);
+    } 
+}
