208ead784a8845f2502a6e2f73ae6271663c2baa
Vidudaya Bandara <vnbandara@gmail.com>
2013-09-11 12:57:50 +0530

    popup.html edited add some Jquery styles

--- Project Source Codes/Chrome Extension/Tag Highlighter/js/init.js
+++ Project Source Codes/Chrome Extension/Tag Highlighter/js/init.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 	$('#topic').fadeIn(3000);
+	$("#highlight,#Annotator,#Annotator2").hide();
+	 
+	$('#quickhighlight').mouseenter(function(){
+		$("#highlight").show(1000);
+	});
+	$('#temparyAnnotate').mouseenter(function(){
+		$("#Annotator").show(1000);
+	});
+	$('#advancedAnnotate').mouseenter(function(){
+		$("#Annotator2").show(1000);
+	});
+	$('#quickhighlight').mouseleave(function(){
+		$("#highlight").hide(1000);
+	});
+	$('#temparyAnnotate').mouseleave(function(){
+		$("#Annotator").hide(1000);
+	});
+	$('#advancedAnnotate').mouseleave(function(){
+		$("#Annotator2").hide(1000);
+	});
+	
+	
 });
