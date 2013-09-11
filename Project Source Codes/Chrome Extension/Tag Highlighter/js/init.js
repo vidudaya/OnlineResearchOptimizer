41751378e145cbe81fb45eb889c74d76b210a3a8
Vidudaya Bandara <vnbandara@gmail.com>
2013-09-11 22:30:03 +0530

    popup.html edited the button styles. add some Jquery styles

--- Project Source Codes/Chrome Extension/Tag Highlighter/js/init.js
+++ Project Source Codes/Chrome Extension/Tag Highlighter/js/init.js
@@ -3,21 +3,27 @@ $(document).ready(function(){
 	$("#highlight,#Annotator,#Annotator2").hide();
 	 
 	$('#quickhighlight').mouseenter(function(){
-		$("#highlight").show(1000);
+		$(this).addClass("buttonStyle2");
+		$("#highlight").delay(1000).show(1000);
 	});
 	$('#temparyAnnotate').mouseenter(function(){
-		$("#Annotator").show(1000);
+		$(this).addClass("buttonStyle2");
+		$("#Annotator").delay(1000).show(1000);
 	});
 	$('#advancedAnnotate').mouseenter(function(){
-		$("#Annotator2").show(1000);
+		$(this).addClass("buttonStyle2");
+		$("#Annotator2").delay(1000).show(1000);
 	});
 	$('#quickhighlight').mouseleave(function(){
+		$(this).removeClass("buttonStyle2");
 		$("#highlight").hide(1000);
 	});
 	$('#temparyAnnotate').mouseleave(function(){
+		$(this).removeClass("buttonStyle2");
 		$("#Annotator").hide(1000);
 	});
 	$('#advancedAnnotate').mouseleave(function(){
+		$(this).removeClass("buttonStyle2");
 		$("#Annotator2").hide(1000);
 	});
 	
