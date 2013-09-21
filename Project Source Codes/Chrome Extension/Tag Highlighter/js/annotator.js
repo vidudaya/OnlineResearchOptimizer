104880210ed158b45719dc8441217f3cad22c277
Vidudaya Bandara <vnbandara@gmail.com>
2013-09-21 21:32:43 +0530

    Weekly report 5 added

--- Project Source Codes/Chrome Extension/Tag Highlighter/js/annotator.js
+++ Project Source Codes/Chrome Extension/Tag Highlighter/js/annotator.js
@@ -5,29 +5,39 @@
 */
 //alert("start annotating");
 
+injectRelatedLinks();
+removeListnersFromBodyElements();
+runAnnotator();
+
 //This will inject a HTML link element to the currently focused web page. This action will be used to 
 // simplyfy the downloading facility.
-var link = document.createElement("link");
-link.href = "http://assets.annotateit.org/annotator/v1.2.5/annotator.min.css";
-link.type = "text/css";
-link.rel = "stylesheet";
-document.getElementsByTagName("head")[0].appendChild(link);
-
+function injectRelatedLinks(){
+	var link = document.createElement("link");
+	link.href = "http://assets.annotateit.org/annotator/v1.2.5/annotator.min.css";
+	link.type = "text/css";
+	link.rel = "stylesheet";
+	document.getElementsByTagName("head")[0].appendChild(link);
+	return true;// code executed successfully	
+}
 
 //This part will remove the event listners added by the simple highlighter
-var divs = document.getElementsByTagName('body');
-	for(var d in divs) { 
-			try{			 
-				if (divs[d].removeEventListener) {
-					 divs[d].removeEventListener('mouseup', callHighlight);
-				} 
-			}catch(err){
-				alert(err.message);
-			}
-	}
-
+function removeListnersFromBodyElements(){
+	var divs = document.getElementsByTagName('body');
+		for(var d in divs) { 
+				try{			 
+					if (divs[d].removeEventListener) {
+						 divs[d].removeEventListener('mouseup', callHighlight);
+					} 
+				}catch(err){
+					alert(err.message);
+				}
+		}
+	return true;// code executed successfully	
+}
 //activate and run the annotation facility. 	
-var $jq = jQuery.noConflict();// avoiding the javaScript and JQuery '$' mark conficts.
-$jq(document.body).annotator();
-
+function runAnnotator(){
+	var $jq = jQuery.noConflict();// avoiding the javaScript and JQuery '$' mark conficts.
+	$jq(document.body).annotator();
+	return true;// code executed successfully	
+}
 //alert("done adding facility");
