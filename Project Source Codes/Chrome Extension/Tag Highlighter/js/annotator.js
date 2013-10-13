4e14bb98e3a3587579f9c2d1c7106936b2f23f68
Vidudaya Bandara <vnbandara@gmail.com>
2013-10-13 07:59:51 +0530

    dependency injection

--- Project Source Codes/Chrome Extension/Tag Highlighter/js/annotator.js
+++ Project Source Codes/Chrome Extension/Tag Highlighter/js/annotator.js
@@ -17,6 +17,17 @@ function injectRelatedLinks(){
 	link.type = "text/css";
 	link.rel = "stylesheet";
 	document.getElementsByTagName("head")[0].appendChild(link);
+	
+	var fileref=document.createElement('script');
+	fileref.setAttribute("type","text/javascript");
+	fileref.setAttribute("src", "http://assets.annotateit.org/annotator/v1.1.0/annotator-full.min.js");
+	document.getElementsByTagName("head")[0].appendChild(fileref);
+	
+	var jqref=document.createElement('script');
+	jqref.setAttribute("type","text/javascript");
+	jqref.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js");
+	document.getElementsByTagName("head")[0].appendChild(jqref);
+	
 	return true;// code executed successfully	
 }
 
@@ -26,7 +37,9 @@ function removeListnersFromBodyElements(){
 		for(var d in divs) { 
 				try{			 
 					if (divs[d].removeEventListener) {
+						
 						 divs[d].removeEventListener('mouseup', callHighlight);
+						// alert("listner removed......");
 					} 
 				}catch(err){
 					alert(err.message);
