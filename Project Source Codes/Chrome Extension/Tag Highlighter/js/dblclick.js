97a163a2c4b5c62569bb2ae7494f0fa081ce6a68
Vidudaya Bandara <vnbandara@gmail.com>
2013-10-13 11:33:06 +0530

    dictioanary fuctionality implementation

---
+++ Project Source Codes/Chrome Extension/Tag Highlighter/js/dblclick.js
@@ -0,0 +1,98 @@
+ 
+function setupExtDoubleClick(websiteUrl, dictionary, showFirstEntry, areaClass, maxAllowedWords, target) {
+    setupDoubleClick(websiteUrl, dictionary, areaClass, maxAllowedWords, target,
+        function(websiteUrl, dictionary, showFirstEntry, lookup) { return websiteUrl + "search/" + (dictionary ? dictionary + "/" : "") + (showFirstEntry ? "direct/" : "") + "?q=" + lookup; });
+}
+
+function setupDoubleClick(websiteUrl, dictionary, showFirstEntry, areaClass, maxAllowedWords, target, urlCallback) {
+	//warning message for developers
+	if (!websiteUrl || !dictionary) {
+		alert("Please specify required parameters (websiteUrl and dictionary) to setupDoubleClick()")
+		return;
+	}
+	
+	//shows the definition layer
+	var showLayer = function(e) {
+        var translateDictionary = dictionary;
+        var node = e.target || e.srcElement;
+        while (node != null) {
+            var lang = node.getAttribute("dict");
+            if (lang) {
+                translateDictionary = lang;
+                break;
+            }
+            if (node.parentNode == node.ownerDocument) {
+                break;
+            }
+            node = node.parentNode;
+        }
+        e.preventDefault();
+        var lookup = getSelectedText();
+        lookup = lookup.replace(/[\.\*\?;!()\+,\[:\]<>^_`\[\]{}~\\\/\"\'=]/g, " ");
+        lookup = lookup.replace(/\s+/g, " ");
+        if (lookup != null && lookup.replace("/\s/g", "").length > 0) {
+        	//disable the double-click feature if the lookup string
+        	//exceeds the maximum number of allowable words
+        	if (maxAllowedWords && lookup.split(/[ -]/).length > maxAllowedWords)
+				return;
+
+        	//append the layer to the DOM only once
+			if ($("#definition_layer").length == 0) {
+	    		var imageUrl = websiteUrl + "external/images/doubleclick/definition-layer.gif";
+				$("body").append("<div id='definition_layer' style='position:absolute; cursor:pointer;'><img src='" + imageUrl + "' alt='' title=''/></div>");
+			}
+			
+			//move the layer at the cursor position
+			$("#definition_layer").map(function() {
+				$(this).css({'left' : e.pageX-30, 'top' : e.pageY-40});
+			});
+			
+			//open the definition popup clicking on the layer
+			$("#definition_layer").mouseup(function(e) {
+				e.stopPropagation();
+				openPopup(lookup, translateDictionary);
+	         });
+        } else {
+        	$("#definition_layer").remove();
+        }
+	};
+
+    //opens the definition popup
+    var openPopup = function(lookup, translateDictionary) {
+        var searchUrl;
+        if (urlCallback)
+            searchUrl = urlCallback(websiteUrl, translateDictionary, showFirstEntry, lookup);
+        else
+            searchUrl = websiteUrl + "search/" + (translateDictionary ? translateDictionary + "/" : "") + (showFirstEntry ? "direct/" : "") + "?q=" + lookup;
+        
+        if (target) {
+            if (target == "self") {
+                window.location.href = searchUrl;
+                return;
+            }
+        	var popup = window.open(searchUrl, target, "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,copyhistory=no,width=915,height=760,top=300,left=300");
+        	if (popup)
+        		popup.focus();
+        } else {
+        	window.open(searchUrl, 'cup_lookup');
+        }
+		/*window.location.href = searchUrl;*/
+    };
+	
+	var area = areaClass ? "." + areaClass : "body";
+	$(area).mouseup(showLayer);
+}
+
+/*
+ * Cross-browser function to get selected text
+ */
+
+function getSelectedText(){
+    if(window.getSelection)
+        return window.getSelection().toString();
+    else if(document.getSelection)
+        return document.getSelection();
+    else if(document.selection)
+        return document.selection.createRange().text;
+    return "";
+}
--- Project Source Codes/Chrome Extension/Tag Highlighter/js/init.js
+++ Project Source Codes/Chrome Extension/Tag Highlighter/js/init.js
@@ -6,36 +6,46 @@
 // set the initial css and JQuery styles
 $(document).ready(function(){
 	$('#topic').fadeIn(3000);
-	$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div").hide();
+	$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div,#dictionary").hide();
 	 
 	$('#quickHighlight').mouseenter(function(){
-		$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div").hide();
+		$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div,#dictionaryDiv").hide();
 		$(this).addClass("buttonStyle2");
 		$("#quickHighlightDiv").delay(1000).show(1000);
-		$("#temparyAnnotate,#advancedAnnotate").removeClass("buttonStyle2");
+		$("#temparyAnnotate,#advancedAnnotate,#dictionaryButton").removeClass("buttonStyle2");
 	});
 	$('#temparyAnnotate').mouseenter(function(){
-		$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div").hide();
+		$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div,#dictionaryDiv").hide();
 		$(this).addClass("buttonStyle2");
 		$("#AnnotatorDiv").delay(1000).show(1000);
-		$("#quickHighlight,#advancedAnnotate").removeClass("buttonStyle2");
+		$("#quickHighlight,#advancedAnnotate,#dictionaryButton").removeClass("buttonStyle2");
 	});
 	$('#advancedAnnotate').mouseenter(function(){
-		$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div").hide();
+		$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div,#dictionaryDiv").hide();
 		$(this).addClass("buttonStyle2");
 		$("#Annotator2Div").delay(1000).show(1000);
-		$("#quickHighlight,#temparyAnnotate").removeClass("buttonStyle2");
+		$("#quickHighlight,#temparyAnnotate,#dictionaryButton").removeClass("buttonStyle2");
+	});
+	$('#dictionaryButton').mouseenter(function(){
+		$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div,#dictionaryDiv").hide();
+		$(this).addClass("buttonStyle2");
+		$("#dictionaryDiv").delay(1000).show(1000);
+		$("#quickHighlight,#temparyAnnotate,#Annotator2Div").removeClass("buttonStyle2");
 	});
 	$('#quickHighlight').mouseleave(function(){
 		//$(this).removeClass("buttonStyle2");
-		$("#AnnotatorDiv,#Annotator2Div").hide();
+		$("#AnnotatorDiv,#Annotator2Div,#dictionaryDiv").hide();
 	});
 	$('#temparyAnnotate').mouseleave(function(){
 		//$(this).removeClass("buttonStyle2");
-		$("#quickHighlightDiv,#Annotator2Div").hide(); 
+		$("#quickHighlightDiv,#Annotator2Div,#dictionaryDiv").hide(); 
 	});
 	$('#advancedAnnotate').mouseleave(function(){
 		//$(this).removeClass("buttonStyle2");
-		$("#quickHighlightDiv,#AnnotatorDiv").hide(); 
+		$("#quickHighlightDiv,#AnnotatorDiv,#dictionaryDiv").hide(); 
+	});
+	$('#dictionaryButton').mouseleave(function(){
+		//$(this).removeClass("buttonStyle2");
+		$("#quickHighlightDiv,#AnnotatorDiv,#Annotator2Div").hide(); 
 	});
 });
